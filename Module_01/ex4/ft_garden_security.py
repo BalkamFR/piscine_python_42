05b83a604ec1d760f5e58571ebacd21c29c8fe9b
Pacome PILAZ <pacomepilaz@gmail.com>
2026-02-12 22:15:00 +0100

    add ex 3 4

--- Module_01/ex4/ft_garden_security.py
+++ Module_01/ex4/ft_garden_security.py
@@ -1,40 +1,61 @@
 class Garden_Security:
 
 	def __init__(self, name_plant:str, starting_height_plant:int, starting_age_plant:int):
-		if (starting_height_plant < 0):
-			print(f"Invalid operation attempted: height {starting_height_plant}cm [REJECTED]")
-			print("Security: Negative height rejected")
-			return
-		if (starting_age_plant < 0):
-			print(f"Invalid operation attempted: age {starting_age_plant}old [REJECTED]")
-			print("Security: Negative age rejected")
-			return
-		if (starting_height_plant >= 0 & starting_age_plant >= 0):	
-			self.name_plant:str = name_plant
-			self.starting_height_plant:int = starting_height_plant + 0.0
-			self.starting_age_plant:int = starting_age_plant
-			self.day_plant:int = 1
-			print(f"Plant created: {self.name_plant}")
-	def age(self)->None:
-		self.starting_age_plant += 1
-		self.day_plant += 1
+		
+		res:int
 
-	def grow(self)->None:
-		self.starting_height_plant += 1
+		res = self.set_height(starting_height_plant)
+		if(res == 1):
+			return
+		res = self.set_age(starting_age_plant)
+		if(res == 1):
+			return
+		self.__name_plant:str = name_plant
+		print(f"Plant created: {self.__name_plant}")
+
+	def set_height(self, height_update:int)->int:
+		
+		if (height_update < 0):
+			self.__starting_height_plant = -1
+			print(f"Invalid operation attempted: height {height_update}cm [REJECTED]")
+			print(f"Security: Negative height rejected")
+			return(1)
+		else:
+			self.__starting_height_plant = height_update
+			return(0)
+	
+	def set_age(self, age_update:int)->int:
+		
+		if (age_update < 0):
+			self.__starting_age_plant = -1
+			print(f"Invalid operation attempted: age {age_update}cm [REJECTED]")
+			print(f"Security: Negative age rejected")
+			return(1)
+		else:
+			self.__starting_age_plant = age_update
+			return(0)
+
+	def get_height(self):
+		return(self.__starting_height_plant)
+	
+	def get_age(self):
+		return(self.__starting_age_plant)
 
 	def get_info(self):
-		return (f"{self.name_plant}: {self.starting_height_plant}cm, {self.starting_age_plant} days old")
+		return(f"Current plant: {self.__name_plant} ({self.get_height()}cm, {self.get_age()} days)")
 	
 	def print_data_plant(self):
-		print(f"Current plant: {self.name_plant} ({self.starting_height_plant}cm, {self.starting_age_plant} days)")
+		if (self.__starting_height_plant == -1 or self.__starting_age_plant == -1):
+			print("Plant is not define")
+			return
+		else:
+			print(self.get_info())
 
-
-def add_plant_tab_plants(all_plants:list, name_plant_add:str, height_plant_add:int, age_plant_add):
-	plant = Garden_Security(name_plant_add, height_plant_add, age_plant_add)
-	print(f"Plant created: {name_plant_add}")
-	all_plants.append(plant)
 
 if __name__ == '__main__':
 	print("=== Garden Security System ===")
-	rose = Garden_Security("Rose", 0, 0)
+	rose = Garden_Security("Rose", 0, -10)
 	rose.print_data_plant()
+
+	tulipe = Garden_Security("tulipe", 29, 20)
+	tulipe.print_data_plant()
