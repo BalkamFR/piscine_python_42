73f8455222e4f4aa1e67ef5ae2b75e13cae26160
Pacome Pilaz <papilaz@z3r11p5.42lyon.fr>
2026-02-19 23:09:06 +0100

    finish module01 and start 02

---
+++ Module_02/ex0/ft_first_exception.py
@@ -0,0 +1,23 @@
+def check_temperature(temp_str) -> None:
+    print()
+    print(f"Testing temperature: {temp_str}")
+    try:
+        a = int(temp_str)
+        if a >= 0 and a <= 40:
+            print(f"Temperature {a}°C is perfect for plants!")
+        elif (a < 0):
+            print(f"Error: -{a}°C is too cold for plants (min 0°C)")
+        elif (a > 40):
+            print(f"Error: {a}°C is too hot for plants (max 40°C)")
+
+    except ValueError:
+        print(f"Error: '{temp_str}' is not a valid number")
+
+
+def test_temperature_input() -> None:
+    print("=== Garden Temperature Checker ===")
+    check_temperature(25)
+    check_temperature("abc")
+    check_temperature(100)
+    check_temperature(-50)
+    print("\nAll tests completed - program didn't crash!")
---
+++ Module_02/ex1/ft_different_errors.py
@@ -0,0 +1,4 @@
+def	garden_operations(temp_str)->None:
+
+def	test_error_types()->None:
+	
